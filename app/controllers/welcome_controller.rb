4234472d667a44487fee380392826cd1b2559b8c
Naz Khanom <nk_91@hotmail.co.uk>
2017-03-09 12:28:29 +0000

    redirects users to list of adventures

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
-  def show; end
+
+  def show
+    redirect_to adventures_path if user_signed_in?
+  end
 end
