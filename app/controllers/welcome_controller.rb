12c07974c6d89a9f6a6c3fcfb44009085aa2068b
Naz Khanom <nk_91@hotmail.co.uk>
2017-04-13 10:45:28 +0100

    Addresses last pull request

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
-
-  def show
-    redirect_to adventures_path if user_signed_in?
-  end
 end
