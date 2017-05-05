7952adab00b2d45920336d9be1c906e02ace33a8
Naz Khanom <nk_91@hotmail.co.uk>
2017-05-05 16:18:13 +0100

    Fixes and styles thumbnails

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
+# frozen_string_literal: true
 
 class PictureUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   version :thumb do
-  process :resize_to_fill => [50, 50]
-end
+    process resize_to_fill: [50, 50]
+  end
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -22,7 +22,8 @@ end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #   # ActionController::Base.helpers.asset_path("fallback/" +
+  #   #   [version_name, "default.png"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
@@ -50,5 +51,4 @@ end
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
