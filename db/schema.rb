fb361b7985a4cd5a39eeb096705e831aa8005f0c
Naz Khanom <nk_91@hotmail.co.uk>
2017-05-12 16:53:36 +0100

    Adds achieved to adventures table

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309113600) do
+ActiveRecord::Schema.define(version: 20170512152016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "adventures", force: :cascade do |t|
-    t.string   "name",        null: false
+    t.string   "name",                        null: false
     t.text     "description"
     t.string   "picture"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "achieved",    default: false
   end
 
   create_table "users", force: :cascade do |t|
